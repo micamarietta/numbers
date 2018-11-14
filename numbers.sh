87548c5accfbe60b24e8767ed0f5b8d9c7c0876d
micamarietta <44626975+micamarietta@users.noreply.github.com>
2018-11-13 21:24:45 -0800

    Update numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -5,13 +5,13 @@ echo "Give me a positive number"
 read number
 N=1
 while [ $N -le $number ]
-			do
-							 if [ $((N%2)) -eq 0 ]
-							 then
-							 				echo $N " Even"
-							 else
-							 				echo $N " Odd"
-		           fi
-							 N=$((N+1))
-			done
+	do
+		if [ $((N%2)) -eq 0 ]
+		then
+			echo $N " Even"
+		else 
+			echo $N " Odd"
+		fi
+		N+$((N+1))
+	done
 echo "Thank you"
