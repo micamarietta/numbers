8af17e539633e07a6f4504567d0abb79324915ab
micamarietta <44626975+micamarietta@users.noreply.github.com>
2018-11-13 21:23:08 -0800

    Update numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -1,1 +1,17 @@
-
+#! /bin/bash
+# numbers.sh 
+#Mica Marietta
+echo "Give me a positive number"
+read number
+N=1
+while [ $N -le $number ]
+			do
+							 if [ $((N%2)) -eq 0 ]
+							 then
+							 				echo $N " Even"
+							 else
+							 				echo $N " Odd"
+		           fi
+							 N=$((N+1))
+			done
+echo "Thank you"
